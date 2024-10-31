3dd53ae1ddf9163fd8cbaae87b6ea990f2062257
HAMMAD123RAZA <01hammadraza@gmail.com>
2024-10-31 05:43:51 -0700

    'updation'

--- backend/server.js
+++ backend/server.js
@@ -1,53 +1,77 @@
+// Import statements
 import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv';
+
 import { User } from './models/UserModel.js';
-import dotenv from 'dotenv'; // Import dotenv
 import { CreateProduct } from './controllers/CreateProduct.js';
 import { filProduct, GetProduct, GetProducts } from './controllers/GetProducts.js';
 import { login, signUp } from './controllers/UserController.js';
 import { delProduct } from './controllers/DelProduct.js';
 import { createOrder } from './controllers/CreateOrder.js';
 
+// Configure environment variables
 dotenv.config();
 
 const app = express();
-app.use(express.json());
-
-
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(cors());
 
-app.use(express.urlencoded({ extended: true }));
-//db.js
-mongoose.connect('mongodb://localhost:27017',{
-    dbName:"Water",
-}).then(()=>{
-    console.log('db connected')
-}).catch((err)=>{
-    console.log('err in connection to db:',err)
-})
+// Database connection function
+const connectDb = async () => {
+    try {
+        await mongoose.connect('mongodb+srv://01hammadraza:mymongodbatlas@waterservices.yt1p9.mongodb.net/?retryWrites=true&w=majority&appName=WaterServices', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            dbName: "Water"
+        });
+        console.log('DB connected');
+    } catch (error) {
+        console.log('Error in DB connection:', error);
+    }
+};
 
-app.post('/create',CreateProduct)
-app.get('/get',GetProducts)
-app.get('/get/:id',GetProduct)
-app.post('/signup',signUp)
-app.get('/login',login)
-app.get('/filter',filProduct)
-app.delete('/delete/:id',delProduct)
-app.post('/sendOrder',createOrder)
+// async function connectDb() {
+//     mongoose.set('strictQuery', true);
+//     const newDb = 'mongodb+srv://01hammadraza:mymongodbatlas@waterservices.yt1p9.mongodb.net/?retryWrites=true&w=majority&appName=WaterServices'
 
-app.get('/',(req,res)=>{
-    res.send('hey there')
-})
+//     mongoose.connect(newDb);
+//     const db = await mongoose.connection;
 
+//     return db.once('open', () => {
+//         console.log('\t\t\t\t\t\t Server is now liveeely connected on: ', newDb);
+//         return true;
+//     });
+// }
+
+// Define routes
+app.post('/create', CreateProduct);
+app.get('/get', GetProducts);
+app.get('/get/:id', GetProduct);
+app.post('/signup', signUp);
+app.get('/login', login);
+app.get('/filter', filProduct);
+app.delete('/delete/:id', delProduct);
+app.post('/sendOrder', createOrder);
+
+// Basic route
+app.get('/', (req, res) => {
+    res.send('hey there');
+});
+
+// Disable caching
 app.use((req, res, next) => {
-    res.set('Cache-Control', 'no-store');  
+    res.set('Cache-Control', 'no-store');
     next();
 });
 
-app.use(express.json({limit:"50mb"}))
-app.use(express.urlencoded({ limit: '50mb', extended: true }));
-
-app.listen(8080, '0.0.0.0', () => {
+// Start server and connect to DB
+app.listen(8080, '0.0.0.0', async () => {
+    await connectDb();
     console.log('Server started on port 8080');
 });
+
+// IvYUe57Tj5Smdjoc
+// mongodb+srv://myusername:IvYUe57Tj5Smdjoc@cluster0.88ykp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
