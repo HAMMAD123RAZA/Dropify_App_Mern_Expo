459187941d84311d60da4e2565e1be18c3f5cf86
Hammad Raza <01hammadraza@gmail.com>
2024-11-03 22:17:40 +0500

    'here'

--- backend/models/UserModel.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const userSch=new mongoose.Schema({
-    userName:String,
-    email:String,
-    password:String
-})
-
-export const User=new mongoose.model('User',userSch)
